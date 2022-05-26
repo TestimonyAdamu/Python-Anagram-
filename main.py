0cb193585e681db685091b8ebc0f1a5f0bfd9265
TestimonyAdamu <105200911+TestimonyAdamu@users.noreply.github.com>
2022-05-26 23:56:14 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Check if a word is an anagrams 
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
+
+
+def find_anagrams(word1, word2):
+    
+        # [assignment] Add your code here
+    if(sorted(word1) == sorted(word2)):
+            print(sorted(word1),sorted(word2))
+            return True
+    return False
+    
+
+print(find_anagrams('listen' , 'silent'))
+    
+
